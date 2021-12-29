dd941bea5b1a79d4361c88f63517961426403b44
MetaHF <yuzhuxiao31@gamil.com>
2021-12-29 18:27:04 +0800

    test1

--- lightServerMusic.js
+++ lightServerMusic.js
@@ -9,6 +9,9 @@ const MIN_LXLVERSION = { major: 0, minor: 5, revision: 10}
 
 const LF_DESCRIPTION = "歌曲$name"
 const LF_CFGPATH = '.\\plugins\\lightDEV\\lightServerMusic\\config.json'
+const LF_PATH = {
+    SM_DIR = '.\\plugins\\lightDEV\\lightServerMusic\\DATA\\'
+}
 
 if(!(File.exists(LF_CFGPATH))){
     init(LF_CFGPATH)
@@ -25,14 +28,51 @@ function checkCFG(path){
         }
     }
 }
-function FileCopyOGG(from,to){
+function FileCopyNoPostfix(from,to){
     //移动所有ogg格式文件到指定目录
 }
 function refresh(cfg){
     let usingNE = cfg.get('usingName')
     let prefix = cfg.get('prefix')
     let content = cfg.get('desciption')
+    let serverRESDIR = cfg.get('serverRES')
+    let pluginData = new File(LF_PATH[SM_DIR],0)
+    let RESData = new File(serverRESDIR+'sounds\\sound_definitions.json',0)
 
+    function FuckOldP(path,json){
+        let pluginDataFR = new File(LF_PATH[SM_DIR],0)
+        let pluginDataFW = new File(LF_PATH[SM_DIR],2)
+        if(!(pluginDataFR.seekTo(0,false)&&pluginDataFW.seekTo(0,false))){
+            Error(3,'无法归零文件指针')
+            return false
+        }
+        if(!(pluginDataFR.readAll(function(result){
+            if(result==null){
+                
+            }
+            for (let i = 0; i < result['music']['main'].length; i++) {
+                let same = false
+                for(let v =0; v<json.length;v++){
+                    if(result['music']['main'][i]['name'] == json[v]){
+                        same = true
+                    }
+                if(!same){
+                    result['music']['main'][i]['name'].
+                    logger.info('已删除文件'+result['music']['main'][i]['name'])
+                }
+
+                }
+                
+            }
+        }))){
+            Error(4,'无法读取文件')
+            return false
+        }
+        function DataW(){
+
+        }
+
+    }
 }
 
 function init(path){
@@ -44,6 +84,7 @@ function init(path){
         cfg.init('usingName',"lightSM")//命名空间
         cfg.init('prefix','LF_')//前缀
         cfg.init('description',LF_DESCRIPTION)//描述
+        cfg.init('serverRES','.\\development_resource_packs\\serverMusicRE\\')
         cfg.init('refresh',false)
         cfg.close
     }
